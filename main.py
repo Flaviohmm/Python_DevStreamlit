ad9f27fe5d649bc3b32e968a925b0797a9edf605
Flavio Macedo <fhenrique609@gmail.com>
2026-03-05 20:20:14 -0300

    Colocando o chat input

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Titulo
 # Input do chat
+
 # A cada mensagem enviada:
     # mostrar a mensagem que o usuario enviou no chat
     # enviar essa mensagem para a IA responder
@@ -7,6 +8,20 @@
 
 # Streamlit - frontend e backend
 
+import os
 import streamlit as st
+from dotenv import load_dotenv
+from openai import OpenAI
 
-st.write("Chatbot com IA")
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+modelo = OpenAI(api_key=OPENAI_API_KEY)
+
+st.write("# Chatbot com IA") # Markdown
+
+texto_usuario = st.chat_input("Digite sua mensagem")
+
+if texto_usuario:
+    print(texto_usuario)
